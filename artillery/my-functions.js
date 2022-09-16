6a1b0331f158f6f73ab407cc2ef9f3a731f3de88
Gerard Snaauw <gerard.snaauw@nedap.com>
2022-09-16 13:20:17 +0200

    add private VC test

---
+++ artillery/my-functions.js
@@ -0,0 +1,16 @@
+//
+// my-functions.js
+//
+module.exports = {
+    setJSONBody: setJSONBody,
+    logHeaders: logHeaders
+}
+
+function setJSONBody(requestParams, context, ee, next) {
+    return next(); // MUST be called for the scenario to continue
+}
+
+function logHeaders(requestParams, response, context, ee, next) {
+    console.log(response.headers);
+    return next(); // MUST be called for the scenario to continue
+}
